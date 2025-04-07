0a6260a4d33034bd36c8e2b113d9fea43fb1d8a4
= <=>
2025-04-08 03:36:35 +0800

    Create 2 builds, 1 for minified

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    minify: "esbuild", // Minify the output using esbuild (default behavior)
-    outDir: "dist", // Output directory
+    minify: process.env.MINIFY === "true" ? "esbuild" : false, // Minify only if the environment variable MINIFY is set to "true"
+    outDir: process.env.MINIFY === "true" ? "dist-minified" : "dist", // Output to different directories
     rollupOptions: {
       input: "./src/init.ts", // Entry file
     },
